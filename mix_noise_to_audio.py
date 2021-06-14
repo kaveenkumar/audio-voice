930f23ce795bc4edaed37aa53f2cae822c300371
Kaveen Kumar <2idli1vada@email.com>
2021-06-14 12:53:43 +0200

    Update mix_noise_to_audio.py

--- mix_noise_to_audio.py
+++ mix_noise_to_audio.py
@@ -17,7 +17,6 @@ import librosa
 import numpy as np
 from scipy.io.wavfile import write
 
-
 logger = logging.getLogger('noise_mixing')
 logger.setLevel(logging.INFO)
 handler = logging.StreamHandler()
@@ -56,7 +55,7 @@ def get_args():
     args = parser.parse_args()
     return args
 
-  
+
 def mix_white_noise(signal, snr_value):
     """
     method1: add additive white gaussian noise
@@ -80,7 +79,8 @@ def mix_white_noise(signal, snr_value):
 
     return signal_noise
 
-  def mix_audio_noise(signal, noise, snr_value):
+
+def mix_audio_noise(signal, noise, snr_value):
     """
     method2: add real world noise from a wav file
     given a signal file, noise (audio) and desired SNR value, this gives the noise (scaled version of noise input) that gives the desired SNR
@@ -112,10 +112,12 @@ def mix_white_noise(signal, snr_value):
 
     return signal_noise
 
+
 # pylint: disable=too-many-locals
 def mix_noise(audio_clean_dir, device_noise_wav, external_noise_dir, snr_value):
     """main function that mixes noise and writes out the augmented audio"""
 
+    # for each noise file in the noise directory, load them over iterations
     for subdir_noise, _, files_noise in os.walk(external_noise_dir):
         for filename_noise in files_noise:
             processed_noise_count = 1
@@ -124,6 +126,7 @@ def mix_noise(audio_clean_dir, device_noise_wav, external_noise_dir, snr_value):
             logger.info(f"NOISE FILE {processed_noise_count} / {files_noise_count} {filename_noise.split('.')[0]}")
             noise, _ = librosa.load(filepath_noise)
 
+            # for each clean audio in input directory, load them over iterations
             for subdir_clean, _, files_clean in os.walk(audio_clean_dir):
                 processed_file_count = 1
                 files_clean_count = len(files_clean)
@@ -136,22 +139,28 @@ def mix_noise(audio_clean_dir, device_noise_wav, external_noise_dir, snr_value):
 
                         signal, _ = librosa.load(filepath_clean)
 
+                        # add AWGN
                         if device_noise_wav:
+                            # mix device noise if provided
                             device_noise = mix_audio_noise(signal, noise, snr_value)
                         else:
+                            # mix white noise if device noise not provided
                             white_noise = mix_white_noise(signal, snr_value=50)
                             device_noise = white_noise
 
+                        # mix real world noise
                         external_noise = mix_audio_noise(signal, noise, snr_value)
 
+                        # add device noise + real world noise
                         signal_noise = device_noise + external_noise
 
+                        # export the augmented audio
                         _, sr = librosa.load(filepath_clean)
                         output_filename = filename_clean.split('.')[0] + "_" + filename_noise.split('.')[0] + "_snr" + str(snr_value) + ".wav"
                         write(Path(audio_clean_dir, output_filename), sr, signal_noise)
                     processed_file_count += 1
 
-                    
+
 if __name__ == '__main__':
     audio_clean_directory = sys.argv[1]
     device_noise_file = sys.argv[2]
