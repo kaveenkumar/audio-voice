fb2589395f85843cc2ee4aceb72563bd8cd0244d
Kaveen Kumar <2idli1vada@email.com>
2021-06-21 23:27:20 +0200

    scale output

--- normalize_audio.py
+++ normalize_audio.py
@@ -8,4 +8,7 @@ fs, x = read('sample_audio.wav')
 #scale down and convert audio into floating point number in range of -1 to 1
 x_norm = np.float32(x)/norm_fact[x.dtype.name]
   
+# x_norm *= INT16_FAC                               # scaling floating point -1 to 1 range signal to int16 range
+# x_norm = np.int16(x)                              # converting to int16 type
+
 write('sample_audio_normalized.wav', fs, x_norm)
